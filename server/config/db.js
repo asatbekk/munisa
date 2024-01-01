5c2e1e6aec36306f850e5b6ce67191e33690e10a
Асатбек Хайруллаев <asatcom26@gmail.com>
2024-01-01 18:20:46 +0600

    sdggfg

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ const connectDB = async () => {
   
   try {
     mongoose.set('strictQuery', false);
-    const conn = await mongoose.connect(process.env.MONGODB_URI ||'mongodb://192.168.1.249:27017/');
+    const conn = await mongoose.connect("mongodb+srv://asatcom26:CvxofHyNZV8of8UO@cluster0.gp5wbn9.mongodb.net/blog"|| process.env.MONGODB_URI  ||'mongodb://192.168.1.249:27017/');
     console.log(`Database Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
